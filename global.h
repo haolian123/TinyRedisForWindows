d9127fcb2b8356d1af5e16fb708c531489c3ce89
hL <1263164021@qq.com>
2023-12-01 23:29:09 +0800

    增加split函数和命令映射表

--- global.h
+++ global.h
@@ -1,12 +1,38 @@
+
 #ifndef GLOBAL
 #define GLOBAL
-
+#include<iostream>
+#include<unordered_map>
+#include<sstream>
 enum SET_MODEL{
     NONE,NX,XX
 };
 
+enum Command{
+    SET,
+    SETNX,
+    SETEX,
+    GET
+};
+
+static std::unordered_map<std::string,enum Command>commandMaps={
+    {"set",SET},
+    {"setnx",SETNX},
+    {"setex",SETEX},
+    {"get",GET}
+};
 
 
+
+static std::vector<std::string> split(const std::string &s, char delimiter=' ') {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
 
 
 
