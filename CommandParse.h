470528da111064b447813915cf677f6a7c4002b0
hL <1263164021@qq.com>
2023-12-02 12:32:53 +0800

    指令解析器

--- CommandParse.h
+++ CommandParse.h
@@ -7,26 +7,35 @@
 #include "global.h"
 #include "RedisHelper.h"
 
-
-
-
 class CommandParser{
 protected:
     static std::shared_ptr<RedisClient>redisClient;
 public:
-    virtual std::string parse(const std::string& line)=0;
+    virtual std::string parse(std::vector<std::string>& tokens)=0;
 };
 std::shared_ptr<RedisClient> CommandParser::redisClient=std::make_shared<RedisClient>();
-// select
-// class SelectParser:CommandParser{
 
-// };
+// select
+class SelectParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<2){
+            return "wrong number of arguments for SELECT.";
+        }
+        int index=0;
+        try{
+            index=std::stoi(tokens[1]);
+        }catch(std::invalid_argument const &e){
+            return tokens[1] +" is not a numeric type";
+        }
+        return redisClient->select(index);
+    }
+};
 
 //set
-class SetParser:CommandParser{
+class SetParser:public CommandParser{
 public:
-    std::string parse(const std::string& line){
-        auto tokens = split(line);
+    std::string parse(std::vector<std::string>& tokens){
         if(tokens.size()<3){
             return "wrong number of arguments for SET.";
         }
@@ -42,59 +51,196 @@ public:
 };
 
 // //setnx
-// class SetnxParser:CommandParser{
-
-// }
-class GetParser:CommandParser{
+class SetnxParser:public CommandParser{
 public:
-    std::string parse(const std::string& line){
-        auto tokens = split(line);
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<3){
+            return "wrong number of arguments for SETNX.";
+        }
+        return redisClient->setnx(tokens[1],tokens[2]);
+    }
+};
+//setex
+class SetexParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<3){
+            return "wrong number of arguments for SETEX.";
+        }
+        return redisClient->setex(tokens[1],tokens[2]);
+    }
+};
+
+//get
+class GetParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
         if(tokens.size()<2){
             return "wrong number of arguments for GET.";
         }
         return redisClient->get(tokens[1]);
     }
 };
+//keys
+class KeysParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        return redisClient->keys();
+    }
+};
 
+// dbsize
+class DBSizeParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        return redisClient->dbsize();
+    }
+};
+
+// exists
+class ExistsParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<2){
+            return "wrong number of arguments for EXISTS.";
+        }
+        tokens.erase(tokens.begin());
+        return redisClient->exists(tokens);
+    }
+};
+//del
+class DelParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<2){
+            return "wrong number of arguments for DEL.";
+        }
+        tokens.erase(tokens.begin());
+        return redisClient->del(tokens);
+    }
+};
+//rename
+class RenameParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<3){
+            return "wrong number of arguments for RENAME.";
+        }
+        return redisClient->rename(tokens[1],tokens[2]);
+    }
+};
+//incr
+class IncrParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<2){
+            return "wrong number of arguments for INCR.";
+        }
+        return redisClient->incr(tokens[1]);
+    }
+};
+//incryby
+class IncrbyParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<3){
+            return "wrong number of arguments for INCRBY.";
+        }
+        int increment=0;
+        try{
+            increment=std::stoi(tokens[2]);
+        }catch(std::invalid_argument const &e){
+            return tokens[2] +" is not a numeric type";
+        }
+        return redisClient->incrby(tokens[1],increment);
+    }
+};
+//incrbyfloat
+class IncrbyfloatParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<3){
+            return "wrong number of arguments for INCRBYFLOAT.";
+        }
+        double increment=0;
+        try{
+            increment=std::stod(tokens[2]);
+        }catch(std::invalid_argument const &e){
+            return tokens[2] +" is not a numeric type";
+        }
+        return redisClient->incrbyfloat(tokens[1],increment);
+    }
+};
+//decr
+class DecrParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<2){
+            return "wrong number of arguments for DECR.";
+        }
+        return redisClient->decr(tokens[1]);
+    }
+};
+
+//decrby
+class DecrbyParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<3){
+            return "wrong number of arguments for DECRBY.";
+        }
+        int increment=0;
+        try{
+            increment=std::stoi(tokens[2]);
+        }catch(std::invalid_argument const &e){
+            return tokens[2] +" is not a numeric type";
+        }
+        return redisClient->decrby(tokens[1],increment);
+    }
+};
+
+//mset
+class MSetParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<2){
+            return "wrong number of arguments for MSET.";
+        }
+        tokens.erase(tokens.begin());
+        return redisClient->mset(tokens);
+    }
+};
+
+//mget
+class MGetParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<2){
+            return "wrong number of arguments for MGET.";
+        }
+        tokens.erase(tokens.begin());
+        return redisClient->mget(tokens);
+    }
+};
+
+//strlen
+class StrlenParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<2){
+            return "wrong number of arguments for STRLEN.";
+        }
+        return redisClient->strlen(tokens[1]);
+    }
+};
+//append
+class AppendParser:public CommandParser{
+public:
+    std::string parse(std::vector<std::string>& tokens){
+        if(tokens.size()<3){
+            return "wrong number of arguments for APPEND.";
+        }
+        return redisClient->append(tokens[1],tokens[2]);
+    }
+};
 #endif
-/*
-
-
-dbsize 
-
-exists
-
-del 
-
-set
-setnx
-setex
-
-rename
-
-get
-
-incr
-incrby
-incrbyfloat
-
-decr
-decrby
-
-mset
-mget
-
-strlen
-
-append
-
-
-
-*/
-
-// class ParserFlyweightFactory{
-// public:
-//     static CommandParser createPaser(int command){
-
-//     }
-// }
